d9981b6269b4669970cc95641fca11ffb95aeaf0
EnginEren <engin.eren@desy.de>
2019-07-05 16:46:57 +0200

    trying to produce output files

--- .travis-ci.d/test.sh
+++ .travis-ci.d/test.sh
@@ -13,7 +13,7 @@ cd /home/ilc/data/Package/StandardConfig/production
 echo "-- Running DDSim ${SIM_MODEL} ..."
 ddsim \
   --inputFiles Examples/bbudsc_3evt/bbudsc_3evt.stdhep \
-  --outputFile bbudsc_3evt_SIM_test_${SIM_MODEL}.slcio \
+  --outputFile /home/ilc/data/Package/bbudsc_3evt_SIM_test_${SIM_MODEL}.slcio \
   --compactFile $lcgeo_DIR/ILD/compact/${SIM_MODEL}/${SIM_MODEL}.xml \
   --steeringFile ddsim_steer.py 
 
@@ -28,7 +28,7 @@ else
 fi
 
 # test presence of output file
-if [ ! -f "bbudsc_3evt_SIM_test_${SIM_MODEL}.slcio" ]
+if [ ! -f "/home/ilc/data/Package/bbudsc_3evt_SIM_test_${SIM_MODEL}.slcio" ]
 then
   ls -lthr
   echo "-- ERROR - DDSim ${SIM_MODEL}: No output file found (bbudsc_3evt_SIM_test_${SIM_MODEL}.slcio)"
@@ -36,43 +36,4 @@ then
 fi
 
 
-##
-## Test Marlin reconstruction
-##
-outputBaseName="bbudsc_3evt_RECNoBG_Test_${REC_MODEL}"
-
-echo "-- Running Marlin ${REC_MODEL} no bg ..."
-Marlin MarlinStdReco.xml \
-	--constant.lcgeo_DIR=$lcgeo_DIR \
-  --constant.DetectorModel=${REC_MODEL} \
-  --constant.OutputBaseName=${outputBaseName} \
-  --constant.RunBeamCalReco=false \
-  --global.LCIOInputFiles=bbudsc_3evt_SIM_test_${SIM_MODEL}.slcio 
-
-marlinStatus=$?
-
-if [[ $marlinStatus != 0 ]]
-then
-  echo "-- ERROR - Marlin ${REC_MODEL} no bg: 3 events test failed"
-  exit $marlinStatus
-else
-  echo "-- Marlin ${REC_MODEL} no bg: test passing !"
-fi
-
-# test presence of different output files
-checkFileList="${outputBaseName}_REC.slcio \
-               ${outputBaseName}_DST.slcio \
-               ${outputBaseName}_PfoAnalysis.root \
-               ${outputBaseName}_AIDA.root"
-for checkFile in ${checkFileList}
-do
-  if [ ! -f ${checkFile} ]
-  then
-    ls -lthr
-    echo "-- ERROR - Marlin ${REC_MODEL} no bg: Missing output file ${checkFile}"
-    exit 1
-  else
-    echo "-- Marlin ${REC_MODEL} no bg: ${checkFile} present ..."
-  fi
-done
 
